00a824cb845bd852321845a12edb9570160a15f4
mis <satoutahhaithem@gmail.com>
2024-03-21 15:16:53 +0100

    add timeSolver Folder

--- run_solvers.sh
+++ run_solvers.sh
@@ -4,9 +4,10 @@ max_parallel_sessions="11"
 cnf_file="instance/${max_parallel_sessions}_session_file_new_format.cnf"
 cnf_file_old_format="instance/${max_parallel_sessions}_session_file.cnf"
 
-timeout_duration="5s"
+timeout_duration="3600s"
 solver_dir="Solvers"
 output_dir="outputs"
+time_dir="TimeSover"
 evalMaxSatFolder="EvalMaxSAT/bin/EvalMaxSAT"
 maxcdclFolder="./MaxCDCL/bin/maxcdcl-scip-maxhs"
 
@@ -20,23 +21,23 @@ chmod +x "$maxcdclFolder"
 
 { 
     time timeout "$timeout_duration" "$solver_dir/maxcdcl_static" "$cnf_file_old_format" > "$output_dir/${max_parallel_sessions}_session_maxcdcl_output.txt"
-} 2> "$output_dir/${max_parallel_sessions}_session_maxcdcl_time.txt"
+} 2> "$time_dir/${max_parallel_sessions}_session_maxcdcl_time.txt"
 
 
 { 
     time timeout "$timeout_duration" "$solver_dir/open-wbo" "$cnf_file_old_format" > "$output_dir/${max_parallel_sessions}_session_open-wbo_output.txt"
-} 2> "$output_dir/${max_parallel_sessions}_session_open-wbo_time.txt"
+} 2> "$time_dir/${max_parallel_sessions}_session_open-wbo_time.txt"
 
 { 
     time timeout "$timeout_duration" "$solver_dir/EvalMaxSAT" "$cnf_file" > "$output_dir/${max_parallel_sessions}_session_EvalMaxSAT_output.txt"
-} 2> "$output_dir/${max_parallel_sessions}_session_EvalMaxSAT_time.txt"
+} 2> "$time_dir/${max_parallel_sessions}_session_EvalMaxSAT_time.txt"
 
 { 
     time timeout "$timeout_duration" python3 "rc2Solver.py" "$cnf_file_old_format" > "$output_dir/${max_parallel_sessions}_session_Rc2_output.txt"
-} 2> "$output_dir/${max_parallel_sessions}_session_Rc2_time.txt"
+} 2> "$time_dir/${max_parallel_sessions}_session_Rc2_time.txt"
 
 { 
     time timeout "$timeout_duration" "$evalMaxSatFolder" --timeUB 0 --minRefTime 5 "$cnf_file" > "$output_dir/${max_parallel_sessions}_session_EvalMaxSAT_SCIP_output.txt"
-} 2> "$output_dir/${max_parallel_sessions}_session_EvalMaxSAT_SCIP_time.txt"
+} 2> "$time_dir/${max_parallel_sessions}_session_EvalMaxSAT_SCIP_time.txt"
 
 echo "Execution times and outputs recorded in respective files."
