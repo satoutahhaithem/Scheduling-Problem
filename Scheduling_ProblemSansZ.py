b1ca68c818c4833e86b9fb5b0e533e604fd4f7bb
satoutahhaithem <satoutahhaithem>
2024-04-09 23:19:05 +0200

    add result of 2022 15 parallel session

--- Scheduling_ProblemSansZ.py
+++ Scheduling_ProblemSansZ.py
@@ -1,220 +1,113 @@
 import numpy as np
-from pysat.formula import *
-from pysat.pb import EncType as pbenc
-from pysat.pb import *
-from pysat.card import *
-from pysat.solvers import *
+from pysat.formula import WCNF, IDPool
+from pysat.pb import PBEnc
+from pysat.card import CardEnc
 from pysat.examples.rc2 import RC2
-from pysat.formula import WCNF
 from data import *
 import sys
-
-
-
-if len(sys.argv) < 2:
-    print("You must write the year of roadef as a second parameter 'for exemple python3 Scheduling_Problem.py 2024'")
+if len(sys.argv) < 3:
+    print("You must write the year roadef as a second parameter and maxparrallel session as third parameter 'for exemple python3 Scheduling_Problem.py 2024 11'")
     sys.exit(1)  # Exit the script with an error code
 
 # Extract the year of roadef from the command-line arguments
 data_set_choice = sys.argv[1]
+max_parallel_sessions = int(sys.argv[2])
 # Check if a command line argument is provided
 if len(sys.argv) > 1:
     data_set_choice = sys.argv[1]
 
 # Choose data set based on the argument
 if data_set_choice == "2024":
-    conference_sessions, slots, papers_range, max_parallel_sessions, working_groups, np, npMax, session_groups = data_for_2024()
+    if (max_parallel_sessions < 9 ): 
+        print ("max_parallel_sessions must more than 9")
+        sys.exit(1) 
+    else:
+        conference_sessions, slots, papers_range, working_groups, np, npMax, session_groups = data_for_2024() 
 elif data_set_choice == "2023":
-    conference_sessions, slots, papers_range, max_parallel_sessions, working_groups, np, npMax, session_groups = data_for_2023()
+    if (max_parallel_sessions < 12 ): 
+        print ("max_parallel_sessions must be more than 12")
+        sys.exit(1) 
+    else:
+        conference_sessions, slots, papers_range, working_groups, np, npMax, session_groups = data_for_2023() 
 elif data_set_choice == "2022":
-    conference_sessions, slots, papers_range, max_parallel_sessions, working_groups, np, npMax, session_groups = data_for_2022()
+    if (max_parallel_sessions < 11 ): 
+        print ("max_parallel_sessions must be more than 11")
+        sys.exit(1) 
+    else:
+        conference_sessions, slots, papers_range, working_groups, np, npMax, session_groups = data_for_2022() 
 elif  data_set_choice == "2021":
-    conference_sessions, slots, papers_range, max_parallel_sessions, working_groups, np, npMax, session_groups = data_for_2021()
+    if (max_parallel_sessions < 5 ): 
+        print ("max_parallel_sessions must be more than 5")
+        sys.exit(1) 
+    else:
+        conference_sessions, slots, papers_range, working_groups, np, npMax, session_groups = data_for_2021()  
 else:
     print("The data available for 2024 , 2023 , 2022 and 2021 only")
     sys.exit(1)
 
 
-
-
-
-length_of_paper_range = len(papers_range)
-globalEncType = EncType.sortnetwrk
-
-
-
-
+# Variable pool for creating new variables
+vpool = IDPool()
 
 # Initialize the weighted clause normal form (WCNF) for the constraints
 constraints = WCNF()
 
-
 # Function to compute variable index for session-slot-paper combination
 def var_x(s, c, l):
-    return (s-1)*slots *length_of_paper_range  + (c-1)*length_of_paper_range + l
-    
-    
+    return vpool.id(f'x_{s}_{c}_{l}')
 
-# Function to decode the variable index back to session, slot, and paper count
-def decode_var_x(x, slots, papers_range_length):
-    # Adjust for 1-indexing
-    x -= 1  
-    l = x % papers_range_length + 1
-    x //= papers_range_length
-    c = x % slots + 1
-    s = x // slots + 1
-    return s, c, l
-
-# Compute the maximum index for var_x
-max_var_x = var_x(conference_sessions, slots, length_of_paper_range)
-
-
-
-y_var = max_var_x  
-
-
+# Function to create and retrieve conflict variable
+def var_y(s1, s2, c):
+    return vpool.id(f'y_{s1}_{s2}_{c}')
 
 # First Constraint: At most one amount of papers chosen for a (session, slot) pair
 for s in range(1, conference_sessions + 1):
     for c in range(1, slots + 1):
-        vars_for_s_c = []
-        for l in range(1 , length_of_paper_range + 1):
-            vars_for_s_c.append(var_x(s, c, l))
-        amo_clause = CardEnc.atmost(lits=vars_for_s_c, bound=1, top_id=y_var,encoding=globalEncType) 
-        y_var=amo_clause.nv
-        constraints.extend(amo_clause.clauses)
-####################################################################################        
-
-
-
-
+        vars_for_s_c = [var_x(s, c, l) for l in range(1, len(papers_range) + 1)]
+        constraints.extend(CardEnc.atmost(vars_for_s_c, 1).clauses)
 
 # Second Constraint: Subdivision of a session into slots covers all the papers in the session
-for s in range(1, conference_sessions +1):
-    aux_vars = []  
-    aux_weight = []  
-    for c in range(1,slots + 1):
-        for l in range(1,length_of_paper_range+1):
-            aux_vars.append(var_x(s, c, l))
-            aux_weight.append(papers_range[l-1])
-    eq_clause = PBEnc.equals(lits=aux_vars, weights=aux_weight,bound=np[s-1], top_id=y_var, encoding=pbenc.sortnetwrk)
-    y_var=eq_clause.nv
-    constraints.extend(eq_clause.clauses)
-####################################################################################
+for s in range(1, conference_sessions + 1):
+    aux_vars = [var_x(s, c, l) for c in range(1, slots + 1) for l in range(1, len(papers_range) + 1)]
+    aux_weights = [papers_range[l-1] for c in range(1, slots + 1) for l in range(1, len(papers_range) + 1)]
+    constraints.extend(PBEnc.equals(lits=aux_vars, weights=aux_weights, bound=np[s-1]).clauses)
 
-
-
-
-
-# Third Constraint : The subdivision respects the maximum length of each slot
+# Third Constraint: Respects the maximum length of each slot
 for s in range(1, conference_sessions + 1):
     for c in range(1, slots + 1):
-        for l in range(1,length_of_paper_range+1):
-            if papers_range[l-1] > npMax[c-1]:
-               # I create a constraint using the negation of x when l exceeds npMax(c).
+        for l, paper in enumerate(papers_range, 1):
+            if paper > npMax[c-1]:
                 constraints.append([-var_x(s, c, l)])
-####################################################################################
-                
 
-
-
-# Fourth Constraint: Number of parallel sessions is not exceeded for each slot
 for c in range(1, slots + 1):
     x_vars = []
     for s in range(1, conference_sessions + 1):
-        for l in range(1,length_of_paper_range+1):
-            x_vars.append(var_x(s,c,l))
-    atmost_clause = CardEnc.atmost(lits=x_vars, bound=max_parallel_sessions, top_id=y_var, encoding=globalEncType)
-    y_var=atmost_clause.nv
-    constraints.extend(atmost_clause.clauses)
-####################################################################################
-
-
-
-
-# Implementing the equivalence transformation for session-slot (z variable)
-# for s in range(1, conference_sessions + 1):
-#     for c in range(1, slots + 1):
-#         z_var = var_z(s, c)
-#         x_vars=[]
-#         for l in range(1,length_of_paper_range+1):
-#             x_vars.append(var_x(s, c, l))
-#         or_clause = x_vars + [z_var]
-#         constraints.append(or_clause)
-
-#         for x in x_vars:
-#             constraints.append([-z_var, -x])
-####################################################################################
-            
-
-
-
-
+        for l in range(1, len(papers_range) + 1):
+            x_vars.append(var_x(s, c, l))
+    constraints.extend(CardEnc.atmost(x_vars, max_parallel_sessions).clauses)
+    
 # Soft Constraints to Minimize Working-Group Conflicts
-
-# Iterate over all possible pairs of sessions (s1, s2), ensuring s1 is less than s2 to avoid duplicates
 for s1 in range(1, conference_sessions + 1):
-
-    for s2 in range(s1 + 1, conference_sessions + 1):  # Ensure s1 < s2
-        # Identify common working groups between the two sessions     
+    for s2 in range(s1 + 1, conference_sessions + 1):
         common_groups = set(session_groups[s1 - 1]).intersection(session_groups[s2 - 1])
-
-        # For each slot, check if the common groups between these two sessions lead to a conflict
         for c in range(1, slots + 1):
-            for g in common_groups:
-                for l in range(1 , length_of_paper_range + 1):
-                    # Create a new variable for each potential conflict (increment y_var)
-                    y_var = y_var + 1
-                    # Add a soft constraint for this potential conflict with a weight of 1.
-                    # This means the solver will try to avoid this situation but can still accept it at a cost
-                    constraints.append([-y_var], weight=1)  
-                    # Hard Constraint : Add a constraint to indicate a conflict if both sessions s1 and s2 are scheduled in the same slot c. 
-                    constraints.append([var_x(s1,c,l),var_x(s2,c,l),y_var])
-####################################################################################
+            if common_groups:
+                y_var = var_y(s1, s2, c)
+                constraints.append([-y_var], weight=1)
+                for l in range(1, len(papers_range) + 1):
+                    constraints.append([-var_x(s1, c, l), -var_x(s2, c, l), y_var])
 
+# Write constraints to a file
+constraints.to_file(f"instance/sansZ/{data_set_choice}/{max_parallel_sessions}_session_file.wcnf")
 
-
-# Specific Constraint for Session 34:
-# This constraint ensures that session 34 is assigned only to slots 5, 6, or 7.
-# for i in range (1,5):
-#     constraints.append([var_z(34,i)])
-# ####################################################################################
-
-constraints.to_file("instance/"+data_set_choice+"/"+str(max_parallel_sessions)+"_session_file.wcnf")
-
-
-
-
-# Assuming other parts of your code (constraint definitions, SAT model setup) are correctly implemented
-
-def display_assignments_by_slot_with_counts(model, slots, papers_range, conference_sessions):
-    print("enter display assignement")
-    slot_assignments = {c: {} for c in range(1, slots + 1)}  # Initialize dictionaries for each slot
-
-    # Processing the model to populate slot assignments
-    for var in range(max_var_x):
-        modelI= model[var]
-        if modelI > 0:
-            s, c, l = decode_var_x(model[var], slots, length_of_paper_range)
-            print(f"  Conference Session {s} in slot {c} with {papers_range[l-1]} papers ")          
-        
-
-
-# print(constraints)
-# with RC2(constraints, solver="Cadical153") as solver:
-#     for model in solver.enumerate():
-#         print ("enter solver")
-#         print('Model has cost:', solver.cost)
-#         # print('Model:', solver.model)
-
+# Solver setup and solution
 with RC2(constraints, solver="Cadical153") as solver:
     for model in solver.enumerate():
         print('Model has cost:', solver.cost)
         # print('Model:', solver.model)
 
-        display_assignments_by_slot_with_counts(model, slots, papers_range, conference_sessions)
+        # display_assignments_by_slot_with_counts(model, slots, papers_range, conference_sessions)
         break  
-
 def convert_cnf_format(old_file_path, new_file_path):
     with open(old_file_path, 'r') as old_file, open(new_file_path, 'w') as new_file:
         for line in old_file:
@@ -228,9 +121,8 @@ def convert_cnf_format(old_file_path, new_file_path):
                 new_file.write(line)
 
 # Specify the old and new file paths
-old_file_path = "./instance/"+data_set_choice+"/"+str(max_parallel_sessions)+"_session_file.wcnf"
-new_file_path = "./instance/"+data_set_choice+"/"+str(max_parallel_sessions)+'_session_file_new_format.wcnf'
+old_file_path = "./instance/"+data_set_choice+"sansZ/"+str(max_parallel_sessions)+"_session_file.wcnf"
+new_file_path = "./instance/"+data_set_choice+"sansZ/"+str(max_parallel_sessions)+'_session_file_new_format.wcnf'
 
 # Call the function to convert the file format
 convert_cnf_format(old_file_path, new_file_path)
-
